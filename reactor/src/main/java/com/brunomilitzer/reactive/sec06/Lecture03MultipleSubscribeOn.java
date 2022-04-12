847395ad21755eefb286f65270eabe55dc169541
brunomilitzer <bruno.militzer@est.tech>
2022-04-12 21:17:34 +0100

    Added Threading & Schedulers

---
+++ reactor/src/main/java/com/brunomilitzer/reactive/sec06/Lecture03MultipleSubscribeOn.java
@@ -0,0 +1,34 @@
+package com.brunomilitzer.reactive.sec06;
+
+import com.brunomilitzer.reactive.util.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lecture03MultipleSubscribeOn {
+
+    public static void main(String[] args) {
+
+        Flux<Object> flux = Flux.create(fluxSink -> {
+            printThreadName("create");
+            fluxSink.next(1);
+        })
+                .subscribeOn(Schedulers.newParallel("vins"))
+                .doOnNext(i -> printThreadName("next " + i));
+
+        Runnable runnable = () -> flux
+                .doFirst(() -> printThreadName("first2"))
+                .subscribeOn(Schedulers.boundedElastic())
+                .doFirst(() -> printThreadName("first1"))
+                .subscribe(v -> printThreadName("sub " + v));
+
+        for (int i = 0; i < 2; i++) {
+            new Thread(runnable).start();
+        }
+
+        Util.sleepSeconds(5);
+    }
+
+    private static void printThreadName(String msg) {
+        System.out.println(msg + "\t\t: Thread : " + Thread.currentThread().getName());
+    }
+}
---
+++ reactor/src/main/java/com/brunomilitzer/reactive/sec06/Lecture04SubscribeOnMultipleItems.java
@@ -0,0 +1,36 @@
+package com.brunomilitzer.reactive.sec06;
+
+import com.brunomilitzer.reactive.util.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lecture04SubscribeOnMultipleItems {
+
+    public static void main(String[] args) {
+
+        Flux<Object> flux = Flux.create(fluxSink -> {
+                    printThreadName("create");
+
+                    for (int i = 0; i < 20; i++) {
+                        fluxSink.next(i);
+                        Util.sleepSeconds(1);
+                    }
+                    fluxSink.complete();
+
+                    fluxSink.next(1);
+                })
+                .doOnNext(i -> printThreadName("next " + i));
+
+        flux.subscribeOn(Schedulers.boundedElastic())
+                .subscribe(v -> printThreadName("sub " + v));
+
+        flux.subscribeOn(Schedulers.parallel())
+                .subscribe(v -> printThreadName("sub " + v));
+
+        Util.sleepSeconds(5);
+    }
+
+    private static void printThreadName(String msg) {
+        System.out.println(msg + "\t\t: Thread : " + Thread.currentThread().getName());
+    }
+}
